69a7616f6927acc961ad0e16d77036520e4b2ea4
Samiullah Khawaja <sami.khawaja@gmail.com>
2019-07-25 21:20:53 +0200

    Integrate 'gst-launch' tool.

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=/media/skhawaja/linux_disk/android/sdk
+export GSTREAMER_ROOT_ANDROID=/media/skhawaja/linux_disk/work/anrdoid/gstreamer/gstreamer-1.0-android-universal-1.16.0
+export PATH=/media/skhawaja/linux_disk/android/sdk/ndk-bundle:$PATH
+export NDK_ROOT=/media/skhawaja/linux_disk/android/sdk/ndk-bundle
